8373082597f6cbedb53ab1c18fd48e999f112437
Juan D. Cardozo <jcardozo@ucsc.edu>
2016-02-23 16:29:19 -0800

    Bug Fix

--- controllers/default.py
+++ controllers/default.py
@@ -177,10 +177,11 @@ def getItems(items):
 
 def getMatchLists(input_list, match_list, curr_item):
     curr_interest_list = getItems(curr_item.intersts)
-    for row in input_list:
-        input_interst_list = getItems(input_list.intersts);
-        for index in interest_list:
-            if((curr_interest_list[index] == input_list.offer) and (==))
+  #  for row in input_list:
+     #   input_interst_list = getItems(input_list.intersts);
+       # for index in interest_list:
+          #  if((curr_interest_list[index] == input_list.offer):
+           #    return
             
     return
 
