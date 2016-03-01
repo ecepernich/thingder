8c4bccb25596c6a11ff5e2dab2cef553ad88d698
ecepernich <ecepernich@gmail.com>
2016-02-29 23:26:50 -0800

    bug fixes + save before merging andy’s location sort

--- models/db1.py
+++ models/db1.py
@@ -7,7 +7,7 @@ db.define_table('posts',
                 Field('offers'),
                 Field('category', requires=IS_IN_SET(categories)),
                 Field('image', 'upload'),
-                Field('User_ID', 'reference auth_user'),
+                Field('User_ID', 'reference auth_user', readable=False, writable=False),
                 auth.signature)
                 
 db.define_table('messages',
@@ -15,19 +15,19 @@ db.define_table('messages',
                 Field('User_ID2'),
                 auth.signature)
 db.define_table('rating',
-                Field('User_ID', 'reference auth_user'),
+                Field('User_ID', 'reference auth_user', readable=False, writable=False),
                 Field('Rcount', 'float'),
                 Field('score', 'float'),
                 Field('rating', 'float'))
 db.rating.Rcount.default = 1;
 db.define_table('votes',
-                Field('Rater','reference auth_user'),
+                Field('Rater','reference auth_user', readable=False, writable=False),
                 Field('Ratee','reference auth_user'),
                 Field('Vtype'),
                 Field('score','float'))
 
 db.define_table('profile',
-                Field('User_ID','reference auth_user'),
+                Field('User_ID','reference auth_user', readable=False, writable=False),
                 Field('image','upload',default='defaultprofile.png'),
                 Field('zip_code'),
                 Field('age','integer'),
